41cfb644e32c5923f33cf9a92cfec5b48669e682
akaJengo <35347523+akaJengo@users.noreply.github.com>
2020-12-16 15:26:27 -0500

    Update Heuristics.java

--- Heuristics.java
+++ Heuristics.java
@@ -1,6 +1,6 @@
 /**
  * Heuristics Berliner, Hans (1999), The System: A World Champion's Approach to
- * Chess, Gambit Publications, ISBN 1-901983-10-2 P 1 K 3.2 B 3.33 R 5.1 Q 8.8
+ * Chess, Gambit Publications, ISBN 1-901983-10-2 P 1.0 K 3.2 B 3.33 R 5.1 Q 8.8
  * King 4.0
  * 
  * 
@@ -9,14 +9,14 @@ public class Heuristics {
 
     int wScore, bScore;
 
-    public int evaluate(Board b, boolean isBlack) {
+    public int evaluate(Piece b, boolean isBlack) {
         wScore = 0;
         bScore = 0;
         int totalPieces = 100;
 
         for (int i = 0; i < totalPieces; i++) {
-            wScore += b.val;
-            bScore += b.val;
+            wScore += 1;
+            bScore += 1;
         }
         if (isBlack) {
             return bScore - wScore;
