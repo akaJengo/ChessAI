0eb516c070aa031e56320b559129b75e817499ca
akaJengo <35347523+akaJengo@users.noreply.github.com>
2020-12-17 16:28:05 -0500

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -29,40 +29,77 @@ public class Queen extends Piece {
     }
 
     @Override
-    public boolean[][] getMoves(int x, int y) {
+    public boolean[][] getMoves(int x, int y, Board board) {
         this.moves = new boolean[8][8];
 
-        // Rook moves
-        for (int i = 0; i < moves.length; i++) {
-            if (y + i <= 8) {
-                this.moves[x][y + i] = true;
-            }
-            if (Math.abs(y - i) >= 0) {
-                this.moves[x][Math.abs(y - i)] = true;
-            }
-            if (x + i <= 8) {
-                this.moves[x + i][y] = true;
-            }
-            if (Math.abs(x - i) >= 0) {
-                this.moves[Math.abs(x - i)][y] = true;
-            }
-        }
-        // Bishop moves
+        // Pos diag down
         for (int i = x + 1, j = y + 1; (i <= moves.length) && (j <= moves.length); i++, j++) {
-            this.moves[i][j] = true;
+            if (board.board[i][j] != null) {
+                break;
+            } else {
+                this.moves[i][j] = true;
+            }
         }
         // Pos diag up
         for (int i = x + 1, j = y - 1; (i <= moves.length) && (j >= 0); i++, j--) {
-            this.moves[i][j] = true;
+            if (board.board[i][j] != null) {
+                break;
+            } else {
+                this.moves[i][j] = true;
+            }
         }
         // neg diag down
         for (int i = x - 1, j = y + 1; (i >= 0) && (j <= moves.length); i--, j++) {
-            this.moves[i][j] = true;
+            if (board.board[i][j] != null) {
+                break;
+            } else {
+                this.moves[i][j] = true;
+            }
         }
         // neg diag up
         for (int i = x - 1, j = y - 1; (i >= 0) && (j >= 0); i--, j--) {
-            this.moves[i][j] = true;
+            if (board.board[i][j] != null) {
+                break;
+            } else {
+                this.moves[i][j] = true;
+            }
         }
-        return this.moves;
+        for (int i = 0; i < moves.length; i++) {
+            if (y + i <= 8) {
+                if (board.board[x][y + i] != null) {
+                    break;
+                } else {
+                    this.moves[x][y + i] = true;
+                }
+            }
+        }
+        for (int i = 0; i < moves.length; i++) {
+            if (Math.abs(y - i) >= 0) {
+                if (board.board[x][Math.abs(y - i)] != null) {
+                    break;
+                } else {
+                    this.moves[x][y + i] = true;
+                }
+            }
+        }
+        for (int i = 0; i < moves.length; i++) {
+            if (x + i <= 8) {
+                if (board.board[x + i][y] != null) {
+                    break;
+                } else {
+                    this.moves[x + i][y] = true;
+                }
+            }
+        }
+        for (int i = 0; i < moves.length; i++) {
+            if (Math.abs(x - i) >= 0) {
+                if (board.board[Math.abs(x - i)][y] != null) {
+                    break;
+                } else {
+                    this.moves[Math.abs(x - i)][y] = true;
+                }
+            }
+        }
+        return this.moves; 
     }
 }
