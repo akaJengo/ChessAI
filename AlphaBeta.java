d6260749872fcb9b41baae395e37efeeb206bc53
Aidan <55853976+aidanLarock@users.noreply.github.com>
2020-12-18 19:40:28 -0500

    Add files via upload

--- AlphaBeta.java
+++ AlphaBeta.java
@@ -10,28 +10,94 @@ import java.util.Queue;
 public class AlphaBeta {
 
     Node head;          // Initial puzzle node
+    Node best;
     
     Heuristics h;
     Board bestBstate;
     
     Queue<Board> q;      // Queues and Array Lists for search algoritms
+    Queue<Board> qW;      // Queues and Array Lists for search algoritms
 
 
-    AlphaBeta(Board b) {
+    public AlphaBeta(Board b) {
         h = new Heuristics();
         q = new LinkedList<>();
-        allMoves(b);
+        qW = new LinkedList<>();
+        head = new Node(b,0.0);
+        Node save = head;
+        int depth = 0;
+        while(depth<8){
+            if(depth % 2 == 0){
+                allMoves(b,false);
+            }else{
+                 allMoves(b,true);
+            }
+            boolean color = depth%2==0;
+            addToNode(head,color);
+            q = new LinkedList<>();
+            if(head.sibling!=null){
+                head = head.sibling;
+            }else{
+            head = head.child;
+            depth++;
+            }
+        }
+        Node best = new Node(null,0.0);
+        best = findBestOutput(save,0,0,best);
+        while(best.parent.parent!=null){
+            best = best.parent;
+        }
+        
     }
     
-    private void allMoves(Board b){
+    public Board getBest(){
+        return best.board;
+    }
+    
+    private void addToNode(Node h,boolean color){
+        Heuristics scoreFinder = new Heuristics();
+        double score = scoreFinder.evaluate(h.board);
+        if(color == true){
+        Board add = q.remove();
+        Node newC = new Node(add, score);
+
+        Node c = newC;
+        h.child = c;
+
+        while(!q.isEmpty()){
+            add = q.remove();
+            newC = new Node(add, score);
+            c.sibling = newC;
+            c = c.sibling;
+        }
+        }else{
+            Board add = qW.remove();
+            Node newC = new Node(add, score);
+            
+            Node c = newC;
+            h.child = c;
+            c.parent = h;
+
+            while(!qW.isEmpty()){
+                add = qW.remove();
+                newC = new Node(add, score);
+                c.sibling = newC;
+                c.parent = h;
+                c = c.sibling;
+            } 
+        }
+    }
+    
+    private void allMoves(Board b,boolean color){
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
-                doMove(b,i,j);
+                doMove(b,i,j,color);
             }
         }
     }
     
     private Board copy(Board b, Board temp){
+        temp = new Board();
         for (int i = 0; i < b.board.length; i++) {
             for (int j = 0; j < b.board[0].length; j++) {
                 temp.board[i][j] = b.board[i][j];
@@ -40,113 +106,46 @@ public class AlphaBeta {
         return temp; 
     }
 
-    private void doMove(Board b, int fromx, int fromy) {
+    private void doMove(Board b, int fromx, int fromy, boolean color) {
         Board newB = null;
         boolean[][] best = null;
-        best = b.board[fromx][fromy].getMoves(fromx, fromy, newB);
+        try{
+        best = b.board[fromx][fromy].getMoves(fromx, fromy, b);
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
                 if(best[i][j]==true){
                     newB = copy(b,newB);
-                    newB.board[fromx][fromy] = newB.board[i][j];
-                    newB.board[i][j] = null;
-                    q.add(newB);
-                }
-            }
-        }
-    }
-    
-    private void doMoves(boolean[][] allMoves){
-        
-    }
-    
-    private boolean[][] bestMove(Board b,int x,int y){
-        double bestScore = 0.0;
-        boolean[][] best = null;
-        best = b.board[x][y].getMoves(x, y, b);
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if(best[i][j]==true){
-                    Board tempB = new Board();
-                    tempB = copy(b,tempB);
-                    h = new Heuristics();
-                    double score = h.evaluate(x,y,i,j,tempB);
-                    if(score>bestScore){
-                        bestScore = score;
-                        b.board[x][y] = b.board[i][j];
-                        b.board[i][j] = null;
+                    newB.board[i][j] = newB.board[fromx][fromy];
+                    newB.board[fromx][fromy] = null;
+                    if(newB.board[i][j].white==color){
+                        q.add(newB);
+                    }else{
+                        qW.add(newB);
                     }
                 }
             }
         }
+        }catch(Exception e){
+            
+        }
+    }
+
+    private Node findBestOutput(Node save,double score, double bestScore, Node best) {
+        if (save == null){ 
+            return best; 
+        }
+
+        score = score+save.score;
+  
+        findBestOutput(save.child, score,bestScore,best); 
+        if(Math.abs(score)>bestScore){
+            bestScore = Math.abs(score);
+            best = save;
+        }
+        score = 0;
+        findBestOutput(save.sibling, score,bestScore,best);
         return best;
     }
 
 
-    public Board alphaBetaSearch(Board b, int depth) {
-        for (int i = 0; i < b.board.length; i++) {
-            double curr = maxValue(b, Double.MIN_VALUE, Double.MAX_VALUE, depth); 
-        }
-        return bestBstate; 
-    }
-
-    private double maxValue(Board b, double alpha, double beta, int depth) {
-        val = Double.MIN_VALUE;
-        if (depth == 0) {
-            double r = h.evaluate(b, true);
-            return r;
-        }
-        for (int i = 0; i < b.board.length; i++) { // need to be for each child of the node, i.e for move in moves.
-            for (int j = 0; j < b.board[0].length; j++) {
-                if (b.board[i][j] != null) {
-                    b = doMove(b, b.board[i][j].getMoves(i, j, b), i, j);
-                    double t = minValue(b, alpha, beta, depth - 1);
-                    if (t < val) {
-                        val = t; 
-                        bestBstate = copy(b); 
-                    }
-                    b = undoMove(b, i, j, toX, toY);
-                    if (val >= beta) {
-                        //bestBstate = doMove(b, b.board[i][j].getMoves(i, j, b), i, j);
-                        //undoMove(b, i, j, toX, toY);
-                        return val;
-                    }
-                    if (alpha < val) {
-                        alpha = val; 
-                    }
-                }
-            }
-        }
-        return val;
-    }
-
-    private double minValue(Board b, double alpha, double beta, int depth) {
-        val = Double.MAX_VALUE;
-        if (depth == 0) {
-            double r = h.evaluate(b, false);
-            return r; 
-        }
-        for (int i = 0; i < b.board.length; i++) {
-            for (int j = 0; j < b.board[0].length; j++) {
-                if (b.board[i][j] != null) {
-                    b = doMove(b, b.board[i][j].getMoves(i, j, b), i, j);
-                    double g = maxValue(b, alpha, beta, depth - 1);
-                    if (g > val) {
-                        val = g; 
-                        bestBstate = copy(b); 
-                    }
-                    b = undoMove(b, i, j, toX, toY);
-                    if (val <= alpha) {
-                        //bestBstate = doMove(b, b.board[i][j].getMoves(i, j, b), i, j);
-                        //undoMove(b, i, j, toX, toY);
-                        return val;
-                    }
-                    if (beta < val) {
-                        beta = val; 
-                    }
-                }
-            }
-        }
-        return val;
-    }
 }
